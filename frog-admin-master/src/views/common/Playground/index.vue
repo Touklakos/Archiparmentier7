52230bf37941557ea5fa82987d93ba657ad98bfc
Touklakos <brabant.mano99@orange.fr>
2020-03-31 00:13:36 +0200

    yolo

--- frog-admin-master/src/views/common/Playground/index.vue
+++ frog-admin-master/src/views/common/Playground/index.vue
@@ -1,18 +1,15 @@
 <template>
 	<section id="playground">
-		<h1>Playground</h1>
+		<h1>Recherche de cartes</h1>
 		<div style="border: 1px solid grey;padding: 10px;border-radius: 5px">
-			<div style="margin-bottom: 20px">
-				<div>Debounce Input</div>
-				<fx-debounce-input :debounce-time="300" v-model="input"/>
-				<br>
-				v-model: {{input}}
-			</div>
 			<div>
-				<div>Normal Input</div>
+				<div>Recherche Cartes</div>
 				<el-input v-model="eli"/>
 				<br>
-				v-model: {{eli}}
+				{{eli}}
+				<div id="example-1">
+				  <button v-on:click="search('Sorin, Vampire Lord')">Search</button>
+				</div>
 			</div>
 		</div>
 	</section>
@@ -24,17 +21,27 @@
 
 	export default {
 		name: "playground",
-		components: {fxDebounceInput},
 		data() {
 			return {
-				input: 'cyh',
-				eli: 'ok',
+				eli: '',
+				counter: 0,
 			}
 		},
-		methods: {},
-		mounted() {
+		methods: {
+			search: function (query) {
+				alert(query)
+				const api = 'https://api.magicthegathering.io/v1';
+				fetch(api+'cards?name='+query).then(res => res.json()).then(json => console.log(json));
+				alert(api+'/cards?name='+query)
 
+			},
 		},
+		mounted() {
+			axios
+		      .get('https://api.magicthegathering.io/v1')
+		      .then(response => (this.eli = response));
+		},
+
 	}
 </script>
 
