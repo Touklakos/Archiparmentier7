ab4c62be4bc1f845a5485a6e47db27a90c8d1e8d
Touklakos <brabant.mano99@orange.fr>
2020-03-31 12:04:20 +0200

    c'est degellase

--- frog-admin-master/src/views/common/Playground/index.vue
+++ frog-admin-master/src/views/common/Playground/index.vue
@@ -4,14 +4,14 @@
 		<div style="border: 1px solid grey;padding: 10px;border-radius: 5px">
 			<div>
 				<div>Recherche Cartes</div>
-				<el-input v-model="eli"/>
+					<el-input v-model="eli" placeHolder="Bonjour"/>
 				<br>
-				{{eli}}
 				<div id="example-1">
-				  <button v-on:click="search('Sorin, Vampire Lord')">Search</button>
+				  <button v-on:click="search($event, eli)">Search</button>
 				</div>
 			</div>
 		</div>
+		<div id = "reponse"></div>
 	</section>
 </template>
 
@@ -28,18 +28,32 @@
 			}
 		},
 		methods: {
-			search: function (query) {
-				alert(query)
-				const api = 'https://api.magicthegathering.io/v1';
-				fetch(api+'cards?name='+query).then(res => res.json()).then(json => console.log(json));
-				alert(api+'/cards?name='+query)
+			search: function (event, query) {
+				const api = 'https://api.magicthegathering.io/v1/cards'
+				var request = new XMLHttpRequest()
+				query = api+'?name='+query
+				const container = document.getElementById('reponse');
+				container.innerHTML = '';
+
+				request.open('GET', query, true)
+				request.onload = function() {
+				  var data = JSON.parse(this.response)
+				    data.cards.forEach(cards => {
+							if(cards.imageUrl != null) {
+								var img = document.createElement('img');
+								img.src = cards.imageUrl;
+								img.style = "width:200px";
+								container.appendChild(img);
+							}
+				    })
+				}
+
+				request.send()
 
 			},
 		},
 		mounted() {
-			axios
-		      .get('https://api.magicthegathering.io/v1')
-		      .then(response => (this.eli = response));
+
 		},
 
 	}
@@ -48,5 +62,8 @@
 <style lang="scss" scoped>
 	canvas {
 		border: 1px solid black;
+	}
+	reponse {
+		width: 100px;
 	}
 </style>
