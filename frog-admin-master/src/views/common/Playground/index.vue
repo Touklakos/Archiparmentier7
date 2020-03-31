8841b910692c06c272a74364be2404e7a82ac482
Touklakos <brabant.mano99@orange.fr>
2020-03-31 16:56:36 +0200

    izi

--- frog-admin-master/src/views/common/Playground/index.vue
+++ frog-admin-master/src/views/common/Playground/index.vue
@@ -4,26 +4,34 @@
 		<div style="border: 1px solid grey;padding: 10px;border-radius: 5px">
 			<div>
 				<div>Recherche Cartes</div>
-					<el-input v-model="eli" placeHolder="Bonjour"/>
+					<el-input v-model="eli" v-on:keyup.enter="search($event, eli)" placeHolder="Rechercher des cartes Magic"/>
 				<br>
 				<div id="example-1">
 				  <button v-on:click="search($event, eli)">Search</button>
+					<button v-on:click="sauvegarderDeck()">Sauvegarder le Deck</button>
 				</div>
 			</div>
 		</div>
-		<div id = "reponse"></div>
+		<div v-for="(value, name) in deck">
+			{{name}}:{{value}}
+		</div>
+
+		<div id="reponse"></div>
 	</section>
 </template>
 
 <script>
 
 	import fxDebounceInput from "@/components/frogx-ui/debounceInput/index";
+	var FileSaver = require('file-saver');
 
 	export default {
 		name: "playground",
 		data() {
 			return {
 				eli: '',
+				contient: [],
+				deck: new Object,
 				counter: 0,
 			}
 		},
@@ -35,21 +43,63 @@
 				const container = document.getElementById('reponse');
 				container.innerHTML = '';
 
+
+				var contient = []
+				var self = this
+
 				request.open('GET', query, true)
 				request.onload = function() {
 				  var data = JSON.parse(this.response)
 				    data.cards.forEach(cards => {
 							if(cards.imageUrl != null) {
-								var img = document.createElement('img');
-								img.src = cards.imageUrl;
-								img.style = "width:200px";
-								container.appendChild(img);
+							 	if(!contient.includes(cards.name)) {
+									var img = document.createElement('img');
+									img.src = cards.imageUrl;
+									img.style = "width:200px";
+									img.ondblclick = function() {
+
+										alert('izi')
+										self.ajouteCarteDeck(cards.name)
+										alert('ultra izi')
+
+									}
+									container.appendChild(img);
+
+									contient.push(cards.name)
+
+								}
 							}
 				    })
 				}
 
+				this.contient = contient
 				request.send()
 
+			},
+			ajouteCarteDeck: function(name) {
+
+				var nbCard = prompt("Vous voulez ajouter "+ name+" \n Indiquez le nombre d'exemplaire :", "1");
+				if (nbCard == "null" || nbCard == "" || nbCard == "0") {
+					alert("attention champ non-rempli !")
+				} else {
+					this.deck[name] = nbCard
+				}
+			},
+
+			retireCarteDeck: function(name) {
+
+				this.deck[name] -= 1
+
+			},
+			sauvegarderDeck: function(){
+				var blob = new Blob(["//votre deck :\n"], {type: "text/plain;charset=utf-8"});
+				for(var k in this.deck) {
+
+					blob = new Blob([blob,this.deck[k]+" "+k+"\n"], {type: "text/plain;charset=utf-8"});
+
+				}
+
+				FileSaver.saveAs(blob, "Deck Magic.txt");
 			},
 		},
 		mounted() {
@@ -63,7 +113,7 @@
 	canvas {
 		border: 1px solid black;
 	}
-	reponse {
-		width: 100px;
+	#img {
+		height: 200px;
 	}
 </style>
